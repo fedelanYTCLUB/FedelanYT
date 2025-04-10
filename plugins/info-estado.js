4139f55a6835fb87c912e00d02477e18e7500954
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-04-10 16:46:42 -0600

    💙HATSUNE-MIKU💙

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -1,39 +1,60 @@
 import ws from 'ws'
-let handler = async (m, { conn, usedPrefix, isRowner}) => {
-let _muptime
-let totalreg = Object.keys(global.db.data.users).length
-let totalchats = Object.keys(global.db.data.chats).length
-let pp = "https://qu.ax/xFjxD.jpg"
-if (process.send) {
-process.send('uptime')
-_muptime = await new Promise(resolve => {
-process.once('message', resolve)
-setTimeout(resolve, 1000)
-}) * 1000
+
+let handler = async (m, { conn, usedPrefix, isRowner }) => {
+    let _muptime
+    let totalreg = Object.keys(global.db.data.users).length
+    let totalchats = Object.keys(global.db.data.chats).length
+    let pp = "https://i.pinimg.com/736x/7b/c6/95/7bc6955d19ce9fa6e562e634d85c912b.jpg"
+    
+    if (process.send) {
+        process.send('uptime')
+        _muptime = await new Promise(resolve => {
+            process.once('message', resolve)
+            setTimeout(resolve, 1000)
+        }) * 1000
+    }
+
+    let muptime = clockString(_muptime)
+    let users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])]
+    const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+    const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) 
+    const totalUsers = users.length
+    let old = performance.now()
+    let neww = performance.now()
+    let speed = neww - old
+    const used = process.memoryUsage()
+
+    let megumin = `💙 \`\`\`Información - HatsuneMiku\`\`\` 💙\n\n`
+    megumin += `💙 *◜Creador◞* ⇢ (ㅎㅊDEPOOLㅊㅎ)\n`
+    megumin += `❗️ *◜Prefijo◞* ⇢ [ ${usedPrefix} ]\n`
+    megumin += `🌺꙰ *◜Versión◞* ⇢ ${vs}\n`
+    megumin += `🌻꙰ *◜Chats Privados◞* ⇢ ${chats.length - groupsIn.length}\n`
+    megumin += `💥 *◜Total De Chats◞* ⇢ ${chats.length}\n`
+    megumin += `👥️️ *◜Usuarios◞* ⇢ ${totalreg}\n`
+    megumin += `🔒 *◜Chats Privados◞* ⇢ ${chats.length - groupsIn.length}\n`
+    megumin += `📌 *◜Grupos◞* ⇢ ${groupsIn.length}\n`
+    megumin += `🕝 *◜Actividad◞* ⇢ ${muptime}\n`
+    megumin += `🚀 *◜Velocidad◞* ⇢ ${(speed * 1000).toFixed(0) / 1000}\n`
+    megumin += `🌱 *◜SubBots Activos◞* ⇢ ${totalUsers || '0'}`
+
+   
+    await conn.sendMessage(m.chat, {
+        image: { url: pp },
+        caption: miku,
+        mentions: [m.sender],
+        contextInfo: {
+            externalAdReply: {
+                title: 'Estado del Bot',
+                body: 'Hatsune Miku 🎶',
+                thumbnailUrl: pp,
+                sourceUrl: 'https://github.com/Brauliovh3',
+                mediaType: 1,
+                renderLargerThumbnail: true
+            }
+        }
+    }, { quoted: m })
 }
-let muptime = clockString(_muptime)
-let users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
-const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) 
-const totalUsers = users.length;
-let old = performance.now()
-let neww = performance.now()
-let speed = neww - old
-const used = process.memoryUsage()
-let megumin = `💙 \`\`\`Información - HatsuneMiku\`\`\` 💙\n\n`
-megumin += `💙 *◜Creador◞* ⇢ David-Chian\n`
-megumin += `❗️ *◜Prefijo◞* ⇢ [ ${usedPrefix} ]\n`
-megumin += `🌺꙰ *◜Versión◞* ⇢ ${vs}\n`
-megumin += `🌻꙰ *◜Chats Privados◞* ⇢ ${chats.length - groupsIn.length}\n`
-megumin += `💥 *◜Total De Chats◞* ⇢ ${chats.length}\n`
-megumin += `👥️️ *◜Usuarios◞* ⇢ ${totalreg}\n`
-megumin += `🔒 *◜Chats Privados◞* ⇢ ${chats.length - groupsIn.length}\n`
-megumin += `📌 *◜Grupos◞* ⇢ ${groupsIn.length}\n`
-megumin += `🕝 *◜Actividad◞* ⇢ ${muptime}\n`
-megumin += `🚀 *◜Velocidad◞* ⇢ ${(speed * 1000).toFixed(0) / 1000}\n`
-megumin += `🌱 *◜SubBots Activos◞* ⇢ ${totalUsers || '0'}`
-await conn.sendFile(m.chat, pp, 'megumin.jpg', megumin, m, null, rcanal)
-}
+
 handler.help = ['status']
 handler.tags = ['info']
 handler.command = ['estado', 'status', 'estate', 'state', 'stado', 'stats']
@@ -41,8 +62,8 @@ handler.register = true
 export default handler
 
 function clockString(ms) {
-let h = Math.floor(ms / 3600000)
-let m = Math.floor(ms / 60000) % 60
-let s = Math.floor(ms / 1000) % 60
-console.log({ms,h,m,s})
-return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
+    let h = Math.floor(ms / 3600000)
+    let m = Math.floor(ms / 60000) % 60
+    let s = Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
