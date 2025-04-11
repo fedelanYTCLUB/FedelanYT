a3b3054c2f093fb98c8ba4c90e766411f9e89498
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-04-11 08:39:53 -0500

    💙HATSUNE-MIKU💙

--- plugins/rpg-cartera.js
+++ plugins/rpg-cartera.js
@@ -1,12 +1,12 @@
 let handler = async (m, {conn, usedPrefix}) => {
-let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-if (who == conn.user.jid) return error 
-if (!(who in global.db.data.users)) return conn.reply(m.chat, '💙 El usuario no se encuentra en mi base de Datos.', m, fake)
-let user = global.db.data.users[who]
-await m.reply(`${who == m.sender ? `Tienes *${user.cookies} Cookies 🍪* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.cookies} Cookies 🍪* en su Cartera`}. `, null, { mentions: [who] })}
-
-handler.help = ['cookies']
+  let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
+  if (who == conn.user.jid) return error 
+  if (!(who in global.db.data.users)) return conn.reply(m.chat, '💙 El usuario no se encuentra en mi base de Datos.', m, fake)
+  let user = global.db.data.users[who]
+  await m.reply(`${who == m.sender ? `Tienes ${user.cebollines} Cebollines 🌱 en tu Cartera` : `El usuario @${who.split('@')[0]} tiene ${user.cebollines} Cebollines 🌱 en su Cartera`}. `, null, { mentions: [who] })
+}
+handler.help = ['cebollines']
 handler.tags = ['rpg']
-handler.command = ['wallet', 'cartera', 'cookies', 'bal', 'coins']
+handler.command = ['wallet', 'cartera', 'cebollines', 'bal', 'coins']
 handler.register = true 
 export default handler
