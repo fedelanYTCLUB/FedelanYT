9d2836bb2c9f2ad5123099eb4f1a53b1954f2fa5
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-04-12 16:15:45 -0600

    💙HATSUNE-MIKU💙

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -1,5 +1,4 @@
 import ws from 'ws'
-
 let handler = async (m, { conn, usedPrefix, isRowner }) => {
     let _muptime
     let totalreg = Object.keys(global.db.data.users).length
@@ -13,7 +12,6 @@ let handler = async (m, { conn, usedPrefix, isRowner }) => {
             setTimeout(resolve, 1000)
         }) * 1000
     }
-
     let muptime = clockString(_muptime)
     let users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])]
     const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
@@ -23,24 +21,22 @@ let handler = async (m, { conn, usedPrefix, isRowner }) => {
     let neww = performance.now()
     let speed = neww - old
     const used = process.memoryUsage()
-
     let megumin = `💙 \`\`\`Información - HatsuneMiku\`\`\` 💙\n\n`
-    megumin += `💙 *◜Creador◞* ⇢ (ㅎㅊDEPOOLㅊㅎ)\n`
-    megumin += `❗️ *◜Prefijo◞* ⇢ [ ${usedPrefix} ]\n`
-    megumin += `🌺꙰ *◜Versión◞* ⇢ ${vs}\n`
-    megumin += `🌻꙰ *◜Chats Privados◞* ⇢ ${chats.length - groupsIn.length}\n`
-    megumin += `💥 *◜Total De Chats◞* ⇢ ${chats.length}\n`
-    megumin += `👥️️ *◜Usuarios◞* ⇢ ${totalreg}\n`
-    megumin += `🔒 *◜Chats Privados◞* ⇢ ${chats.length - groupsIn.length}\n`
-    megumin += `📌 *◜Grupos◞* ⇢ ${groupsIn.length}\n`
-    megumin += `🕝 *◜Actividad◞* ⇢ ${muptime}\n`
-    megumin += `🚀 *◜Velocidad◞* ⇢ ${(speed * 1000).toFixed(0) / 1000}\n`
-    megumin += `🌱 *◜SubBots Activos◞* ⇢ ${totalUsers || '0'}`
-
+    megumin += `💙 ◜Creador◞ ⇢ (ㅎㅊDEPOOLㅊㅎ)\n`
+    megumin += `❗️ ◜Prefijo◞ ⇢ [ ${usedPrefix} ]\n`
+    megumin += `🌺꙰ ◜Versión◞ ⇢ ${vs}\n`
+    megumin += `🌻꙰ ◜Chats Privados◞ ⇢ ${chats.length - groupsIn.length}\n`
+    megumin += `💥 ◜Total De Chats◞ ⇢ ${chats.length}\n`
+    megumin += `👥️️ ◜Usuarios◞ ⇢ ${totalreg}\n`
+    megumin += `🔒 ◜Chats Privados◞ ⇢ ${chats.length - groupsIn.length}\n`
+    megumin += `📌 ◜Grupos◞ ⇢ ${groupsIn.length}\n`
+    megumin += `🕝 ◜Actividad◞ ⇢ ${muptime}\n`
+    megumin += `🚀 ◜Velocidad◞ ⇢ ${(speed * 1000).toFixed(0) / 1000}\n`
+    megumin += `🌱 ◜SubBots Activos◞ ⇢ ${totalUsers || '0'}`
    
     await conn.sendMessage(m.chat, {
         image: { url: pp },
-        caption: miku,
+        caption: megumin, 
         mentions: [m.sender],
         contextInfo: {
             externalAdReply: {
@@ -54,13 +50,11 @@ let handler = async (m, { conn, usedPrefix, isRowner }) => {
         }
     }, { quoted: m })
 }
-
 handler.help = ['status']
 handler.tags = ['info']
 handler.command = ['estado', 'status', 'estate', 'state', 'stado', 'stats']
 handler.register = true
 export default handler
-
 function clockString(ms) {
     let h = Math.floor(ms / 3600000)
     let m = Math.floor(ms / 60000) % 60
