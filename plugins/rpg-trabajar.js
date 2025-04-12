fef3d98b3e73d5a820a3e29c7403902939d16319
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-04-12 17:55:56 -0600

    💙HATSUNE-MIKU💙

--- plugins/rpg-trabajar.js
+++ plugins/rpg-trabajar.js
@@ -1,5 +1,32 @@
 let cooldowns = {}
 
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
+function segundosAHMS(segundos) {
+  let horas = Math.floor(segundos / 3600)
+  segundos %= 3600
+  let minutos = Math.floor(segundos / 60)
+  segundos %= 60
+  return `${horas ? horas + 'h ' : ''}${minutos ? minutos + 'm ' : ''}${segundos}s`
+}
+
+function toNum(number) {
+  return new Intl.NumberFormat('en-US').format(number)
+}
+
+const trabajo = [
+  "Trabajaste como programador y ganaste",
+  "Trabajaste en una empresa y ganaste",
+  "Trabajaste como freelancer y ganaste",
+  "Trabajaste en un restaurante y ganaste",
+  "Trabajaste como diseñador y ganaste",
+  "Trabajaste como profesor y ganaste",
+  "Trabajaste en construcción y ganaste",
+  "Trabajaste como médico y ganaste"
+]
+
 let handler = async (m, { conn, isPrems }) => {
   let user = globalThis.db.data.users[m.sender]
   let tiempo = 5 * 60
@@ -13,11 +40,9 @@ let handler = async (m, { conn, isPrems }) => {
   let rsl = Math.floor(Math.random() * 5000)
   cooldowns[m.sender] = Date.now()
   
-  
   const monedaText = globalThis.monedas || "XP"
   
   await conn.reply(m.chat, `💙 ${pickRandom(trabajo)} ${toNum(rsl)} ( ${rsl} ) ${monedaText}💫.`, m)
-  
   
   user.exp += rsl
 }
