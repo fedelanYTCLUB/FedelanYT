a8d2fb39035d3831e8342e3c3b47a39619f6f68f
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-04-23 22:06:35 -0600

    💙HATSUNE-MIKU💙

--- plugins/rpg-depositar.js
+++ plugins/rpg-depositar.js
@@ -5,17 +5,17 @@ let user = global.db.data.users[m.sender]
 if (!args[0]) return m.reply('💙 Ingresa la cantidad de *Cebollines 🌱* que deseas Depositar.')
 if ((args[0]) < 1) return m.reply('💙 Ingresa una cantidad válida de *Cebollines 🌱*.')
 if (args[0] == 'all') {
-let count = parseInt(user.chocolates)
-user.chocolates -= count * 1
+let count = parseInt(user.cebollines)
+user.cebollines -= count * 1
 user.bank += count * 1
 await m.reply(`Depositaste *${count} Cebollines 🌱* al Banco.`)
 return !0
 }
 if (!Number(args[0])) return m.reply('💙 La cantidad deve ser un Numero.')
 let count = parseInt(args[0])
-if (!user.chocolates) return m.reply('No tienes *Cebollines 🌱* en la Cartera.')
-if (user.chocolates < count) return m.reply(`Solo tienes *${user.cookies} Cebollines 🌱* en la Cartera.`)
-user.chocolates -= count * 1
+if (!user.cebollines) return m.reply('No tienes *Cebollines 🌱* en la Cartera.')
+if (user.cebollines < count) return m.reply(`Solo tienes *${user.cebollines} Cebollines 🌱* en la Cartera.`)
+user.cebollines -= count * 1
 user.bank += count * 1
 await m.reply(`Depositaste *${count} Cebollines 🌱* al Banco.`)}
 
