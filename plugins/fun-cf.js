1aeb6d4a566fdc26df91a703d224deaf3b678ecf
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-04-24 11:02:30 -0500

    💙HATSUNE-MIKU💙

--- plugins/fun-cf.js
+++ plugins/fun-cf.js
@@ -3,7 +3,7 @@ let users = {};
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [eleccion, cantidad] = text.split(' ');
     if (!eleccion || !cantidad) {
-        return m.reply(`💙💮 Por favor, elige cara o cruz y una cantidad de galletas para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
+        return m.reply(`💙💮 Por favor, elige cara o cruz y una cantidad de micrófonos para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
     }
 
     eleccion = eleccion.toLowerCase();
@@ -13,24 +13,24 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     }
 
     if (isNaN(cantidad) || cantidad <= 0) {
-        return m.reply(`💙💮 Cantidad no válida. Por favor, elige una cantidad de galletas para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
+        return m.reply(`💙💮 Cantidad no válida. Por favor, elige una cantidad de micrófonos para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
     }
 
     let userId = m.sender;
-    if (!users[userId]) users[userId] = { cookies: 100 };
+    if (!users[userId]) users[userId] = { cebollines: 100 };
     let user = global.db.data.users[m.sender];
-    if (user.cookies < cantidad) {
-        return m.reply(`💙💮 No tienes suficientes galletas para apostar. Tienes ${user.cookies} galletas.`);
+    if (user.cebollines < cantidad) {
+        return m.reply(`💙💮 No tienes suficientes micrófonos para apostar. Tienes ${user.cebollines} Micrófonos.`);
     }
 
     let resultado = Math.random() < 0.5 ? 'cara' : 'cruz';
    let mensaje = `💙 La moneda ha caído en `
     if (resultado === eleccion) {
-        user.cookies += cantidad; 
-    mensaje += `*${resultado}* y has ganado *${cantidad} galletas*!`;
+        user.cebollines += cantidad; 
+    mensaje += `*${resultado}* y has ganado *${cantidad} micrófonos*!`;
     } else {
-        user.cookies -= cantidad;
-        mensaje += `*${resultado}* y has perdido *${cantidad} galletas*!`;
+        user.cebollines -= cantidad;
+        mensaje += `*${resultado}* y has perdido *${cantidad} micrófonos*!`;
     }
 
     await conn.reply(m.chat, mensaje, m);
